567792a023f72b591f25d47d3db9717da46ef9bc
VOLL digital <volldigital@VOLLs-MacBook.local>
2018-04-27 12:33:08 +0200

    yoyo

--- app/js/App.js
+++ app/js/App.js
@@ -3,141 +3,90 @@
  */
 class App {
   constructor(props) {
+
+    /*Variabeln*/
     const buttons = document.getElementsByClassName("termin");
-    var datum = "!Nicht Beantwortet!";
-    console.log(buttons);
-    buttons[0].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[0].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[1].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[1].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[2].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[2].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[3].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[3].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[4].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[4].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[9].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[9].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[5].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[5].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[6].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[6].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[7].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[7].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[8].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[8].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[10].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[10].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[11].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[11].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[12].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[12].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[13].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[13].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[14].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[14].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[15].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[15].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[16].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[16].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
-    buttons[17].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      datum = buttons[17].value;
-      console.log("Hier ist das datum:", datum);
-    })
-
     const privacy = document.getElementsByClassName("privacy");
-    console.log(privacy);
-    var privat = "!Nicht Beantwortet!";
-
-    privacy[0].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      privat = privacy[0].value;
-      console.log("Dürfen wir?", privat);
-    })
-
-    privacy[1].addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      privat = privacy[1].value;
-      console.log("Dürfen wir?", privat);
-    })
-
     const absenden = document.getElementById("absenden");
+    const buttonArray = Array.from(buttons);
+    var datum = "!Nicht Beantwortet!";
+    var privat = "!Nicht Beantwortet!";
+    var date;
+    var time;
+    var recolor;
+    console.log(buttonArray);
+    console.log(privacy);
     console.log(absenden);
-    absenden.addEventListener("click", (e) => {
-      console.log("Button wurde gedrückt", this, e);
-      window.alert("Ihr Termin findet am " +datum+ " statt.\n\n Auf unsere Datenschutzfrage haben Sie mit " +privat+ " geantwortet.\n\n Vielen Dank für Ihre Anmeldung.")
-    })
 
+    /*Buttons für Zeit und Wochentagen*/
+    {
+      buttonArray.forEach(button => 
+      {
+        button.addEventListener('click', (e) => 
+        {
+          console.log("Button Click:", e.target.value);
+          datum = e.target.value;
+          buttonArray.forEach(button =>
+          {
+            button.style.backgroundColor= "black";
+            button.style.color= "white";
+          })
+          e.target.style.backgroundColor= "white";
+          e.target.style.color= "black";
+        });
+      })
+    }
 
+    /*Buttons für Datenschutz*/
+    {
+      privacy[0].addEventListener("click", (e) => 
+      {
+        console.log("Button Click:", e.target.value);
+        privat = e.target.value;
+        document.getElementsByTagName("button")[19].style.backgroundColor= "black";
+        document.getElementsByTagName("button")[19].style.color= "white";
+        document.getElementById("b19").style.backgroundColor= "white";
+        document.getElementById("b19").style.color= "black";
+      })
+
+      privacy[1].addEventListener("click", (e) => 
+      {
+        console.log("Button Click:", e.target.value);
+        privat = e.target.value;
+        document.getElementsByTagName("button")[18].style.backgroundColor= "black";
+        document.getElementsByTagName("button")[18].style.color= "white";
+        document.getElementById("b20").style.backgroundColor= "white";
+        document.getElementById("b20").style.color= "black";
+      })
+    }
+
+    /*Button fürs Absenden der Anmeldung*/
+    {
+      absenden.addEventListener("click", (e) => 
+      {
+        console.log("Button Click:", e.target, "Anmelden");
+        if (datum == "!Nicht Beantwortet!")
+          {
+            window.alert("Nicht komplett ausgefüllt!");
+          }else if (privat == "!Nicht Beantwortet!")
+            {
+              window.alert("Nicht komplett ausgefüllt!");
+            }else
+              {
+                console.log("Datum: ", datum, ", ", "Privacy: ", privat );
+                window.alert("Ihr Termin findet am " +datum+ " statt.\n\n Auf unsere Datenschutzfrage haben Sie mit " +privat+ " geantwortet.\n\n Vielen Dank für Ihre Anmeldung.");
+                datum = "!Nicht Beantwortet!";
+                privat = "!Nicht Beantwortet!";
+                recolor = document.querySelectorAll("button[type='button']");
+                const recolorArray = Array.from(recolor);
+                recolorArray.forEach(button =>
+                {
+                  button.style.backgroundColor= "black";
+                  button.style.color="white";
+                })
+              }
+      })
+    }
 
   }
 }
